07cb7aed397edd5380ee7a628f09dbaf3ede437f
Chenst <2453527962@qq.com>
2022-08-26 21:46:47 +0800

    first commit

---
+++ src/main/java/com/cst/TestGit.java
@@ -0,0 +1,11 @@
+package com.cst;
+
+/**
+ * @Author:陈世廷
+ * @Version:1.0
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+    }
+}
